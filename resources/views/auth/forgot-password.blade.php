96fe73839e2dc6808fc1f8d24c14a38b8dca6a9d
Harsh_Shah <harshshah6966@gmail.com>
2025-03-23 12:38:42 +0530

    forgoate password functionality added...

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,97 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> forgot-password</title>
+    <!--/google-fonts -->
+    <link href="//fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;1,400;1,600&display=swap" rel="stylesheet">
+    <!--//google-fonts -->
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{asset('user/assets/css/style-starter.css')}}">
+</head>
+
+<body>
+    <!--/Header-->
+    @include('user.common.header')
+    <!--//Header-->
+    
+    <!-- Theme Switcher Dropdown (Light/Dark) -->
+    <div class="theme-switcher">
+        <select id="theme-switch-dropdown" aria-label="Switch Theme">
+            <option value="light">Light Mode</option>
+            <option value="dark">Dark Mode</option>
+        </select>
     </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <!-- breadcrumb -->
+    <section class="w3l-about-breadcrumb">
+        <div class="breadcrumb-bg breadcrumb-bg-about">
+            <div class="container py-lg-5 py-sm-4">
+                <div class="w3breadcrumb-gids text-center">
+                    <div class="w3breadcrumb-info mt-5">
+                        <h2 class="w3ltop-title pt-4">Get Forgote Your Password</h2>
+                        <ul class="breadcrumbs-custom-path">
+                            <li><a href="{{route('home')}}">Home</a></li>
+                            <li class="active"><span class="fas fa-angle-double-right mx-2"></span> Forgote Password </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
+    </section>
+    <!--//breadcrumb-->
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+    <!-- contact2 -->
+    <section class="w3l-contact-1 w3hny-form-btm py-5" id="login">
+        <div class="contacts-9 py-lg-5 py-md-4">
+            <div class="container">
+                <div class="header-sec text-center mb-5">
+                <h6 class="title-subhny mb-2">Forgot Your Password?</h6>
+                <h3 class="title-w3l">
+                    No worries! Enter your email to reset your password and regain access to your account.
+                </h3>
+
+                </div>
+                @if(session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+                @endif
+                <div class="contactct-fm map-content-9">
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+
+                    <!-- Email Address -->
+                    <div>
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        <button class="btn btn-primary btn-style">
+                            {{ __('Email Password Reset Link') }}
+                        </button>
+                    </div>
+                </form>
+                 </div>
+            </div>
         </div>
-    </form>
-</x-guest-layout>
+    </section>
+    <!-- /contact2 -->
+
+    <!-- footer -->
+    @include('user.common.footer')
+    <!-- //footer -->
+
+    <!-- Js scripts -->
+    <script src="{{asset('user/assets/js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset('user/assets/js/theme-change.js')}}"></script>
+    <!-- Template JavaScript -->
+    <script src="{{asset('user/assets/js/bootstrap.min.js')}}"></script>
+
+</body>
+
+</html>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,114 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+<html lang="en">
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> Reset-Password</title>
+    <!--/google-fonts -->
+    <link href="//fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;1,400;1,600&display=swap" rel="stylesheet">
+    <!--//google-fonts -->
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{asset('user/assets/css/style-starter.css')}}">
+</head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+<body>
+    <!--/Header-->
+    @include('user.common.header')
+    <!--//Header-->
+    
+   
+
+    <!-- breadcrumb -->
+    <section class="w3l-about-breadcrumb">
+        <div class="breadcrumb-bg breadcrumb-bg-about">
+            <div class="container py-lg-5 py-sm-4">
+                <div class="w3breadcrumb-gids text-center">
+                    <div class="w3breadcrumb-info mt-5">
+                        <h2 class="w3ltop-title pt-4">Get Reset Your Password</h2>
+                        <ul class="breadcrumbs-custom-path">
+                            <li><a href="{{route('home')}}">Home</a></li>
+                            <li class="active"><span class="fas fa-angle-double-right mx-2"></span> Reset Password </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
+    </section>
+    <!--//breadcrumb-->
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+    <!-- contact2 -->
+    <section class="w3l-contact-1 w3hny-form-btm py-5" id="login">
+        <div class="contacts-9 py-lg-5 py-md-4">
+            <div class="container">
+                <div class="header-sec text-center mb-5">
+                <h6 class="title-subhny mb-2">Reset Your Password?</h6>
+                <h3 class="title-w3l">
+                    No worries! Enter your email to reset your password and regain access to your account.
+                </h3>
+
+                </div>
+                @if(session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+                @endif
+                <div class="contactct-fm map-content-9">
+                
+                <form method="POST" action="{{ route('password.store') }}">
+                    @csrf
+
+                    <!-- Password Reset Token -->
+                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                    <!-- Email Address -->
+                    <div>
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password" :value="__('Password')" />
+                        <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                        <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                            type="password"
+                                            name="password_confirmation" required autocomplete="new-password" />
+
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        <button class="btn btn-primary btn-style">
+                            {{ __('Reset Password') }}
+                        </button>
+                    </div>
+                </form>
+
+                </div>
+            </div>
         </div>
+    </section>
+    <!-- /contact2 -->
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+    <!-- footer -->
+    @include('user.common.footer')
+    <!-- //footer -->
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
+    <!-- Js scripts -->
+    <script src="{{asset('user/assets/js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset('user/assets/js/theme-change.js')}}"></script>
+    <!-- Template JavaScript -->
+    <script src="{{asset('user/assets/js/bootstrap.min.js')}}"></script>
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+</body>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>

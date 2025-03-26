a778de835d547b543d47dc18830b988ba12ce58a
Harsh_Shah <harshshah6966@gmail.com>
2025-03-26 13:06:31 +0530

    changes done

--- resources/views/user/MyJobs.blade.php
+++ resources/views/user/MyJobs.blade.php
@@ -20,10 +20,6 @@ $count = 1;
 
 
 
-    <!-- Bootstrap JS (optional, depending on the features used) -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
 </head>
 
